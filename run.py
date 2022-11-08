281dc44283c1bb02d27c891f0e61c9ae38634054
murtovski <murta1991@gmail.com>
2022-11-08 18:42:48 +0000

    Created validation to convert to int and return the sales data

--- run.py
+++ run.py
@@ -17,14 +17,19 @@ def get_sales_data():
     """
     Get sales figures from the user
     """
-    print("Please enter sales data from the last market")
-    print("Data should be six numbers, seperated by commas")
-    print("Example: 10, 20, 30, 40, 40, 30")
+    while True:
+        print("Please enter sales data from the last market")
+        print("Data should be six numbers, seperated by commas")
+        print("Example: 10, 20, 30, 40, 40, 30")
 
-    data_str = input("Enter your data here: ")
-   
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        data_str = input("Enter your data here: ")
+    
+        sales_data = data_str.split(",")
+        validate_data(sales_data)
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
 
 
 def validate_data(values):
@@ -34,13 +39,16 @@ def validate_data(values):
     or if there aren't exactly  values.
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}. Please try again.\n")
+        return False
 
+    return True
 
 
 get_sales_data()
